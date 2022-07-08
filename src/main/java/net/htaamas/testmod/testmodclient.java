fea9d32d9f27330f6c68d256799ecd7a2499eff6
HTaamas <huntamas2222@gmail.com>
2022-07-08 19:51:38 +0200

    Initial commit

---
+++ src/main/java/net/htaamas/testmod/testmodclient.java
@@ -0,0 +1,10 @@
+package net.htaamas.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class testmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
